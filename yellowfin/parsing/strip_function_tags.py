7aedb29ccff46bd0004065240a413ce2341998df
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-12-14 11:37:54 +0100

    removed yellowfin

--- yellowfin/parsing/strip_function_tags.py
+++
@@ -1,8 +0,0 @@
-from bllipparser import Tree
-import fileinput
-
-for line in fileinput.input():
-	tree = Tree(line[:-1])
-	for subtree in tree.all_subtrees():
-		subtree.label_suffix = ''
-	print tree
